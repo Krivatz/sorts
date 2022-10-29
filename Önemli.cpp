9fa22c859bd600755edc886822ac76fa2e51b5f5
Krivatz <76409534+Krivatz@users.noreply.github.com>
2022-10-29 16:23:55 +0300

    Add files via upload

---
+++ Önemli.cpp
@@ -0,0 +1,6 @@
+//Quick selection algoritmasini yazamadım. İnternette yaptığım araştırmalar sırasında bir algoritma kopyalıyıp ödeve uygun hale getirdim elde ettiğim quick sort sonucu bana ait olmayan bir koddan meydana gelmiştir.
+
+//1.quick sort 12.45sn
+//2.insertion sort 14.35sn
+//3.selection sort 16sn
+//4. bubble sort 26.97sn
