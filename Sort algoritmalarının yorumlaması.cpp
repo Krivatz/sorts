ec0a79ab09e29efa793d15bd8000f4d4b84eb1dd
Krivatz <76409534+Krivatz@users.noreply.github.com>
2022-10-31 18:34:00 +0300

    Update and rename Önemli.cpp to Sort algoritmalarının yorumlaması.cpp

--- Sort algoritmalarının yorumlaması.cpp
+++ Sort algoritmalarının yorumlaması.cpp
@@ -1,6 +1,11 @@
 //Quick selection algoritmasini yazamadım. İnternette yaptığım araştırmalar sırasında bir algoritma kopyalıyıp ödeve uygun hale getirdim elde ettiğim quick sort sonucu bana ait olmayan bir koddan meydana gelmiştir.
 
 //1.quick sort 12.45sn
+//quick sort bütün sayıları sıralarken parçalama yöntemini kullandığı için en hızlı sıralayan algoritma olmuştur.
+
 //2.insertion sort 14.35sn
 //3.selection sort 16sn
+
+
 //4. bubble sort 26.97sn
+// bubble sort seçtiği bir elemanı dizede ki tüm elemanlar ile teker teker karşılaştırdğı için en yavaş algoritmadır
